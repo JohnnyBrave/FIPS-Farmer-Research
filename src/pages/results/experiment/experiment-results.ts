912777fc6f6c7a35c2b331fb7812ffe50842e67e
laptop\chris <chris.m.clarke@live.co.uk>
2017-11-11 08:33:08 -0800

    ipmrove farmer perception insight design

--- src/pages/results/experiment/experiment-results.ts
+++ src/pages/results/experiment/experiment-results.ts
@@ -24,13 +24,17 @@ export class ExperimentResultsPage {
   focusFarmer: string = "_none";
   activeInsight: string = "_none";
   groPlusSurveyMeta: any = meta;
-  collatedTemplate: any;
+  collatedResults: any = {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public databasePrvdr: DatabaseProvider) {
     this._setInsights()
     this._getData()
-    this._createOptionsMap()
     console.log('groPlus meta', this.groPlusSurveyMeta)
+  }
+
+  iconClick(i){
+    // method when counter person icon clicked
+    console.log('i',i)
   }
 
   _getData() {
@@ -40,8 +44,8 @@ export class ExperimentResultsPage {
     this.databasePrvdr.getSubCollection('Surveys', '8ft5LhC8t9Uo072m34px', "Farmers")
       .subscribe(r => {
         this.results = r
-        this._processResults(r)
         console.log('results', this.results)
+        this._processResults(r)
         this._getMeta(r)
       })
     this.surveys = this.databasePrvdr.getCollection('Surveys')
@@ -68,42 +72,87 @@ export class ExperimentResultsPage {
   }
   _processResults(results) {
     // takes array of json results objects and aggregates into counts of specific values
-    // currently hardcoded for this vis, but could be generalised
-    let collated = {}
+    // first set preloaded values using options map (a bit hardcoded, should be generalised)
+    this.collatedResults = this._createOptionsMap(this.groPlusSurveyMeta)
     for (let r of results) {
       let farmerKey = r._farmerKey
-      if (r.hasOwnProperty('Differences observed')) {
-        for (let crop in r['Differences observed']) {
-          if (r['Differences observed'].hasOwnProperty(crop)) {
-            for (let difference in r['Differences observed'][crop]) {
-              let val = r['Differences observed'][crop][difference]
-              console.log('difference', difference, 'val', val)
-              this.collatedTemplate[crop][difference][val].push(farmerKey)
+      this.collatedResults = this._flatten(this.collatedResults, r, farmerKey)
+    }
+    console.log('collated', this.collatedResults)
+
+  }
+  _flatten(collated, json, pushVal) {
+    // take a current json array and map to corresponding array of values in collated, pushing id value
+    for (let key in json) {
+      if (json.hasOwnProperty(key)) {
+        let val = json[key]
+        if (val != "") {
+          //ignore empty string values
+          if (!collated[key]) { collated[key] = {} }
+          if (typeof val == "string" || typeof val == "number") {
+            // strings or numbers can be mapped directly
+            collated[key] = this._mapValToObject(collated[key], val, pushVal)
+          }
+          if (typeof val == "object") {
+            // objects need to be determined and dealt with. Undefined or null are ignored
+            if (val[0]) {
+              // arrays just populate multiple string values
+              this._mapArrayToObject(collated[key], val, pushVal)
+            }
+            else if (val instanceof Date) {
+              // dates currently ignored but could be collated by day/month/year like in network results
+              console.log('ignoring date objects')
+            }
+            else if (Object.keys(val).length > 0) {
+              // json objects go through same process recursively
+              collated[key] = this._flatten(collated[key], val, pushVal)
             }
           }
         }
       }
+      //   // 
+      //   // 
+      //   // else{console.error('object type unknown',val)}
+      //   // json, arrays or date objects need to be handled recursively
+
     }
-    console.log('collated', this.collatedTemplate)
+    return collated
   }
-  _addToCollated(key) {
+  _mapValToObject(json, value, id) {
+    // push an id to a matching value array in an object, to track frequency of specific results
+    if (!json.hasOwnProperty(value)) { json[value] = [] }
+    json[value].push(id)
+    return json
+  }
+
+  _mapArrayToObject(json, array, id) {
+    // push an id to multiple matching value arrays in an object, to track frequency of specific results
+    for (let el of array) {
+        if (!json.hasOwnProperty(el)) { json[el] = [] }
+        json[el].push(id)
+        return json
+    }
 
   }
-  _createOptionsMap() {
+
+  _createOptionsMap(meta) {
     // need format cropNAME.diffName.diffValue =[]
     // e.g. maize.'flowering time'.earlier=[]
-    let collatedTemplate = {}
-    for (let crop of this.groPlusSurveyMeta["Used with"].options) {
-      collatedTemplate[crop.value] = {}
-      for (let d of this.groPlusSurveyMeta["Differences observed"].options) {
-        collatedTemplate[crop.value][d.value] = {}
+
+    //should be collated with other fuctions from flatten, although beta schema needed
+    // (not diferentiating the differences as sub of used with)
+    let map = { "Differences observed": {} }
+    for (let crop of meta["Used with"].options) {
+      // populate possible crops
+      map["Differences observed"][crop.value] = {}
+      for (let d of meta["Differences observed"].options) {
+        // populate possible subcrop differences
+        map["Differences observed"][crop.value][d.value] = {}
         for (let option of d.subOptions) {
-          collatedTemplate[crop.value][d.value][option.value] = []
+          map["Differences observed"][crop.value][d.value][option.value] = []
         }
       }
     }
-
-    this.collatedTemplate = collatedTemplate
-    console.log('collated template', this.collatedTemplate)
+    return map
   }
 }
